fac3ca409ab50914dc3c17489d34ae6f81a9f7b8
Jonnel <jonnelle@student.21-school.ru>
2024-05-15 16:34:11 +0300

    have to connect

--- src/movingClasses/movingClasses.h
+++ src/movingClasses/movingClasses.h
@@ -8,10 +8,19 @@
 namespace s21 {
  class MovingContext{
   private:
-    std::unique_ptr<s21::MovingStrategy> movingStrategy_;
+    std::unique_ptr<s21::MovingStrategy> movingStrategy{};
+    float moveValue_;
+    char axi_;
+
   public:
+    void setStrategy(std::unique_ptr<MovingStrategy> &&movingStrategy_) {
+      movingStrategy = std::move(movingStrategy_);
+    }
     
- }
+    void strategy() {
+      if(movingStrategy) movingStrategy-> movingVertex(moveValue_, axi_);
+    }
+ };
 }
 
 
