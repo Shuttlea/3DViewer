088cfc9dc24e528e1117faeb0a456c79a3be3cb1
Shuttlea <rockabilly1985@yandex.ru>
2024-05-18 03:59:17 +0300

    scale_is_work

--- build-src-Desktop_x86_darwin_generic_mach_o_64bit-Debug/CMakeFiles/QOpenGLWidget.dir/Model/singleton.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/QOpenGLWidget.dir/Model/singleton.cpp.o: \
-  /Users/shuttlea/TryGitHub/3DViewer_2_0/src/Model/singleton.cpp \
-  /Users/shuttlea/TryGitHub/3DViewer_2_0/src/Model/singleton.h
